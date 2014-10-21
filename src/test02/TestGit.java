b55ea77140ea1498fe4c5b6b8e6b44122b731842
bingo <bingo@bingo-PC>
2014-10-21 16:18:37 +0800

    sth I want to change ,and sth I don't want to change!

--- src/test02/TestGit.java
+++ src/test02/TestGit.java
@@ -5,6 +5,12 @@ public class TestGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git is perfect!");
+		
+		
+		
+		
+		
+		System.out.println("Myself change! Do not want to change!");
 	}
 
 }
--- src/test02/TestGitUpdate.java
+++ src/test02/TestGitUpdate.java
@@ -5,6 +5,10 @@ public class TestGitUpdate {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test update");
+		
+		
+		
+		System.out.println("test change!");
 	}
 
 }
