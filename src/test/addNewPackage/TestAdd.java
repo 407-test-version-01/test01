e5c2d539cd52d1d5f92a5a2749a288ba92295d52
bingo <xhbin1990@126.com>
2014-10-27 21:59:50 +0800

    output line

--- src/test/addNewPackage/TestAdd.java
+++ src/test/addNewPackage/TestAdd.java
@@ -4,7 +4,10 @@ public class TestAdd {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("add new Package!");
+		System.out.println("add new Package!");
+		for(int i = 0; i < 9; i++){
+			System.out.println("the "+ i +" Line" );
+		}
 	}
 
 }
